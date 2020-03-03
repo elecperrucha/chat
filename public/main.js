faa16d5b78230192cee012939097a9ffd6b734d4
Glitch (socketio-chat) <none>
2020-03-03 21:45:26 +0000

    👣📊 Checkpoint ./public/index.html:8634008/86 ./public/main.js:8634008/23

--- public/main.js
+++ public/main.js
@@ -231,7 +231,7 @@ $(function() {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "Welcome to the exerras Chat";
     log(message, {
       prepend: true
     });
