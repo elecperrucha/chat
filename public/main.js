dbb5b48b083cfe32611e6c7cf130aa631b37ef0a
Glitch (coke-chatboxbox) <none>
2022-03-03 21:15:40 +0000

    🏐💺 Checkpoint ./public/main.js:38081116/23

--- public/main.js
+++ public/main.js
@@ -231,7 +231,7 @@ $(function() {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Tikcord IRC chat - #general";
+    var message = "Tikcord IRC chat - #LETS-GO-BEAR";
     log(message, {
       prepend: true
     });
